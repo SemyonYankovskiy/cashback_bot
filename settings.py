a287fe6b6bf29c4aeff5fe7ab7acd999fa1b87b4
SemyonYankovskiy <130610669+SemyonYankovskiy@users.noreply.github.com>
2025-06-04 10:02:11 +0300

    правильный путь для БД

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 API_TOKEN = os.getenv('BOT_TOKEN')
-DB_NAME = "cashback_bot.db"
+DB_NAME = "db/cashback_bot.db"
 
 bot = Bot(token=API_TOKEN, parse_mode='HTML')
 storage = MemoryStorage()
